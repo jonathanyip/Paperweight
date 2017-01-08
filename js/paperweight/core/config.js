02ea826cbaee90b42346f34d912bfa1e63f08e82
Jonathan Yip <jonathangyip@gmail.com>
2017-01-07 21:59:20 -0800

    Added some comments, so I don't forget what is going on.

--- js/paperweight/core/config.js
+++ js/paperweight/core/config.js
@@ -4,16 +4,16 @@
  */
 define({
     // Game setting values
-    GAME_WIDTH: 1280,
-    GAME_HEIGHT: 720,
+    GAME_WIDTH: 1280, // Canvas Width
+    GAME_HEIGHT: 720, // Canvas Height
 
     // Camera setting values
-    CAMERA_MOTION_LERP: 0.05,
+    CAMERA_MOTION_LERP: 0.05, // Lerp value for the camera motion. Lower values are smoother, but slower
 
     // Player physics values
-    PLAYER_MAX_VELOCITY: 300,
-    PLAYER_ACCELERATION: 300,
-    PLAYER_DRAG: 300,
-    PLAYER_REVERSE_DAMP_LERP: 0.2,
-    PLAYER_BOUNCE: 0.5
+    PLAYER_MAX_VELOCITY: 300,      // Max velocity that the player can move
+    PLAYER_ACCELERATION: 300,      // Acceleration that the player has
+    PLAYER_DRAG: 300,              // How fast the player slows down when not accelerating
+    PLAYER_REVERSE_DAMP_LERP: 0.2, // Lerp value for dampening of reverse motion (i.e. pressing up then down quickly)
+    PLAYER_BOUNCE: 0.5             // How much energy in conserved when hitting a collider
 });
--- js/paperweight/stages/world.js
+++ js/paperweight/stages/world.js
@@ -14,22 +14,24 @@ define(["core/game", "core/config"], function(game, Config) {
 
     World.prototype = {
         preload: function() {
+            // Load background images/sprites
             game.load.image("background", "resources/background/paper.jpg");
             game.load.image("player", "resources/sprites/star.png")
         },
         create: function() {
+            // Create a player
             this.background = game.add.tileSprite(0, 0, Config.GAME_WIDTH, Config.GAME_HEIGHT, "background");
             this.background.fixedToCamera = true;
             this.player = game.add.sprite(32, 32, "player");
 
             // Set some player properties
             game.physics.arcade.enable(this.player);
-
             this.player.body.collideWorldBounds = true;
             this.player.body.bounce.set(Config.PLAYER_BOUNCE);
+            this.player.body.drag.set(Config.PLAYER_DRAG);
             this.player.body.maxVelocity.set(Config.PLAYER_MAX_VELOCITY);
 
-            // Add arrow keys and wasd
+            // Add arrow keys and WASD
             this.cursor = game.input.keyboard.addKeys({
                 'up': Phaser.KeyCode.UP, 'down': Phaser.KeyCode.DOWN, 'left': Phaser.KeyCode.LEFT, 'right': Phaser.KeyCode.RIGHT,
                 'W': Phaser.Keyboard.W, 'A': Phaser.Keyboard.A, 'S': Phaser.Keyboard.S, 'D': Phaser.Keyboard.D
@@ -40,15 +42,19 @@ define(["core/game", "core/config"], function(game, Config) {
         },
         update: function() {
             // Handle player movements
+            // Initialize acceleration to zero
             this.player.body.acceleration.set(0);
-            this.player.body.drag.set(Config.PLAYER_DRAG);
 
+            // Do this for every arrow/WASD key
             if(this.cursor.left.isDown || this.cursor.A.isDown) {
+                // If the velocity is in the opposite direction (i.e. a reverse key, like up then down quickly)
+                // Lerp the velocity back towards zero more quickly than if using acceleration alone.
                 if(this.player.body.velocity.x > 0) {
                     this.player.body.velocity.x -= this.player.body.velocity.x * Config.PLAYER_REVERSE_DAMP_LERP;
                     console.log("OpLeft");
                 }
 
+                // Set the acceleration in the left direction
                 this.player.body.acceleration.x -= Config.PLAYER_ACCELERATION;
             }
             if(this.cursor.right.isDown || this.cursor.D.isDown) {
@@ -77,17 +83,20 @@ define(["core/game", "core/config"], function(game, Config) {
             }
 
             // Handle player rotation
+            // Point it towards the mouse pointer
             this.player.rotation = game.physics.arcade.angleToPointer(this.player);
 
             // Handle the background movement
             this.background.tilePosition.set(-game.camera.x, -game.camera.y);
 
             // Handle the camera movement
+            // Camera motion uses lerp so it is smoother
             this.cameraPos.x += (this.player.body.x - this.cameraPos.x) * Config.CAMERA_MOTION_LERP;
             this.cameraPos.y += (this.player.body.y - this.cameraPos.y) * Config.CAMERA_MOTION_LERP;
             game.camera.focusOnXY(this.cameraPos.x, this.cameraPos.y);
         },
         render: function() {
+            // Some debug text...
             game.debug.text("Acceleration: x=" + this.player.body.acceleration.x + "; y=" + this.player.body.acceleration.y, 32, 32);
             game.debug.text("Velocity: x=" + this.player.body.velocity.x + "; y=" + this.player.body.velocity.y, 32, 64);
             game.debug.text("Drag: x=" + this.player.body.drag.x + "; y=" + this.player.body.drag.y, 32, 96);
